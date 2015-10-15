1abd5ead24b9edb7f13236e1d6709d3a634f087a
IT Lab1 <IT Lab1@ITLab1-PC>
2015-10-15 14:06:07 +0200

    try web page with angular

--- src/main/java/ch/css/pomodoro/service/config/PomodoroApplication.java
+++ src/main/java/ch/css/pomodoro/service/config/PomodoroApplication.java
@@ -19,7 +19,10 @@ public class PomodoroApplication extends Application<PomodoroConfiguration> {
 	@Override
 	public void initialize(Bootstrap<PomodoroConfiguration> bootstrap) {
 		super.initialize(bootstrap);
-		bootstrap.addBundle(new AssetsBundle("/web/infoview", "/info", "index.html", "infoview"));
+		bootstrap.addBundle(new AssetsBundle("/web/", "/info", "/infoview/index.html", "infoview"));
+		//bootstrap.addBundle(new AssetsBundle("/web/infoview", "/info", "index.html", "infoview"));
+		bootstrap.addBundle(new AssetsBundle("/web/lib/*", "/lib/", "/lib/*", "lib"));
+		bootstrap.addBundle(new AssetsBundle("/web/infoview/js", "/info/js", null, "js"));
 		bootstrap.addBundle(new AssetsBundle("/web/adminview/", "/admin", "index.html", "adminview"));
 	}
 
---
+++ src/main/resources/web/infoview/js/pomodoro.controller.js
@@ -0,0 +1,9 @@
+angular.module('pomodoro').controller('pomodoroController', function($scope) {
+
+	$scope.tomatoes = [ {
+		'name' : 'Adi the Best',
+	}, {
+		'name' : 'Rahul baby'
+	} ];
+
+});
