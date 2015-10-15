80dcb3958fe2178e6d08bb8e8a781311d3b40ced
sascha <sascha@debian>
2015-10-15 17:49:33 +0200

    introduced seconds to time filter

--- src/main/resources/web/infoview/js/pomodoro.controller.js
+++ src/main/resources/web/infoview/js/pomodoro.controller.js
@@ -1,4 +1,18 @@
-angular.module('pomodoro').controller('pomodoroController', function(pomodoroService, $filter) {
+angular.module('pomodoro').controller('pomodoroController', function(pomodoroService, $filter, $interval, $scope) {
 	var vm = this;
 	vm.users = pomodoroService.loadUsers();
+	
+	var refreshData = function() {
+		vm.users = pomodoroService.loadUsers();
+	};
+
+	var promise = $interval(refreshData, 1000);
+
+	// Cancel interval on page changes
+	$scope.$on('$destroy', function(){
+	    if (angular.isDefined(promise)) {
+	        $interval.cancel(promise);
+	        promise = undefined;
+	    }
+	});
 });
